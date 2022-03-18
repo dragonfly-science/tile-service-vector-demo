9daa8b7856b91d88601bde866bd6af265b092115
xycarto <xycarto@gmail.com>
2022-03-18 19:16:34 +1300

    zoom and webaddress

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const mapBase = new maplibregl.Map({
     container: 'map', // container id
     style: './style/style.json', // style URL
-    center: [166.453857,-45.821143,14], // starting position [lng, lat]
-    zoom: 3 // starting zoom
+    center: [174.0,-41.29], // starting position [lng, lat]
+    zoom: 9 // starting zoom
     });
 
